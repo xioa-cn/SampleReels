e82b2d9f06d6f06f608b8b38edb08e19277ad364
xioa-cn <xioa-cn@users.noreply.github.com>
2025-11-28 05:26:13 +0000

    deploy: ed39afc9f2297bda15d14f1abd655b9202d2cdf0

--- assets/index-DGQvTFhf.js
+++ assets/index-DGQvTFhf.js
@@ -127,7 +127,7 @@
 按需裁剪框架功能，仅保留项目必需的依赖注入、事件聚合、区域路由等核心模块，避免冗余代码。
 支持自定义区域（Region）行为与导航规则，轻松适配复杂界面交互逻辑。
 模块（Module）加载机制可深度扩展，灵活实现按需加载、动态插拔，适配大型应用模块化拆分。
-软件内部数据对外开放模式，内置Garnet、Rpc服务器、共享内存通信订阅，以及多种数据库支持。配合 WPF 数据绑定与命令系统，既能享受 Prism 架构的规范化开发优势，又能摆脱固定框架约束，让架构设计完全贴合业务场景，尤其适合需要深度定制交互逻辑或轻量化部署的项目。`,technologies:[`.net 8.0`,`WPF`,`XPrism`,`HandyControl`,`I18n`,`EFCore`,`PgSql`,`Mysql`,`Sqlite`,`MsSql`],link:`https://github.com/xioa-cn/PressMachineSystem.AutoConfigSystem`,image:oc(`WPF.png`),github:`https://github.com/your-username/chat-app`,status:`completed`},{id:4,title:`WPF DeskTop 授权及解密模块`,description:`—— 企业级安全防护，授权期限灵活定制
+软件内部数据对外开放模式，内置Garnet、Rpc服务器、共享内存通信订阅，以及多种数据库支持。配合 WPF 数据绑定与命令系统，既能享受 Prism 架构的规范化开发优势，又能摆脱固定框架约束，让架构设计完全贴合业务场景，尤其适合需要深度定制交互逻辑或轻量化部署的项目。`,technologies:[`.net 8.0`,`WPF`,`XPrism`,`HandyControl`,`I18n`,`EFCore`,`PgSql`,`Mysql`,`Sqlite`,`MsSql`],link:`https://github.com/xioa-cn/WPF.Admin.Template.Core`,image:oc(`WPF.png`),github:`https://github.com/xioa-cn/WPF.Admin.Template.Core`,status:`completed`},{id:4,title:`WPF DeskTop 授权及解密模块`,description:`—— 企业级安全防护，授权期限灵活定制
 模块采用加密算法（AES + MD5 + Hash）构建企业级安全体系，全面保护软件知识产权。
 支持按业务需求灵活配置授权期限，可自定义永久授权、固定时长授权、订阅式授权等多种模式，同时支持试用期限、续期规则的个性化设定。
 集成加密解密核心功能，涵盖数据加密、密钥安全管理与授权验证流程，适配桌面级部署的安全合规要求，无需从零搭建底层安全逻辑，开箱即用且易于扩展。`,technologies:[`AES`,`Hash`,`MD5`,`Base64`],image:oc(`Auth.jpg`),status:`completed`},{id:5,title:`WPF Naive UI 组件库`,description:`一个 WPF 组件库`,technologies:[`WPF`,`C#`],link:`https://github.com/xioa-cn/NaiveUI.Wpf`,image:oc(`NaiveUI.png`),github:`https://github.com/xioa-cn/NaiveUI.Wpf`,status:`in-progress`},{id:6,title:`WPF + Deepseek AI个人助手`,description:`—— 聚焦纯粹智能对话，打造自然高效交互体验
